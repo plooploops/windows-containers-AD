50db5e58abcfbaed71627ee91d0fc106fbe71bfd
Andy <andy.gee@microsoft.com>
2018-09-18 16:50:27 -0700

    wip insider testing

--- auth-examples/build.10.0.17666.1000.ps1
+++ auth-examples/build.10.0.17666.1000.ps1
@@ -11,11 +11,11 @@ docker build -f IISBase/Dockerfile -t "$repo/iis:windowsservercore-insider-10.0.
 docker build -f web-builder/Dockerfile.windowsservercore-10.0.17666 -t "$repo/4.7.2-windowsservercore-insider-10.0.17666.1000-builder" web-builder
 
 #build individual apps
-docker build -f no-auth/Dockerfile.windowsservercore-10.0.17666 -t "$repo/windows-ad:no-auth-windowsservercore-insider-10.0.17666.1000" no-auth
-docker build -f windows-auth-simple/Dockerfile.windowsservercore-10.0.17666 -t "$repo/windows-ad:simple-windowsservercore-insider-10.0.17666.1000" .
-docker build -f iis-simple/Dockerfile.windowsservercore-10.0.17666 -t "$repo/windows-ad:iis-simple-windowsservercore-insider-10.0.17666.1000" iis-simple
-docker build -f windows-auth-impersonate-frontend/Dockerfile.windowsservercore-10.0.17666 -t "$repo/windows-ad:impersonate-frontend-windowsservercore-insider-10.0.17666.1000" .
-docker build -f windows-auth-impersonate-backend/Dockerfile.windowsservercore-10.0.17666 -t "$repo/windows-ad:impersonate-backend-windowsservercore-insider-10.0.17666.1000" .
-docker build -f windows-auth-impersonate-explicit-frontend/Dockerfile.windowsservercore-10.0.17666 -t "$repo/windows-ad:impersonate-explicit-frontend-windowsservercore-insider-10.0.17666.1000" .
-docker build -f windows-auth-impersonate-groupupn-backend/Dockerfile.windowsservercore-10.0.17666 -t "$repo/windows-ad:impersonate-groupupn-backend-windowsservercore-insider-10.0.17666.1000" .
-docker build -f windows-auth-impersonate-globalasax-backend/Dockerfile.windowsservercore-10.0.17666 -t "$repo/windows-ad:impersonate-globalasax-backend-windowsservercore-insider-10.0.17666.1000" .
+docker build -f "no-auth/Dockerfile.windowsservercore-10.0.17666" -t "$repo/windows-ad:no-auth-windowsservercore-insider-10.0.17666.1000" no-auth
+docker build -f "windows-auth-simple/Dockerfile.windowsservercore-10.0.17666" -t "$repo/windows-ad:simple-windowsservercore-insider-10.0.17666.1000" .
+docker build -f "iis-simple/Dockerfile.windowsservercore-10.0.17666" -t "$repo/windows-ad:iis-simple-windowsservercore-insider-10.0.17666.1000" iis-simple
+docker build -f "windows-auth-impersonate-frontend/Dockerfile.windowsservercore-10.0.17666" -t "$repo/windows-ad:impersonate-frontend-windowsservercore-insider-10.0.17666.1000" .
+docker build -f "windows-auth-impersonate-backend/Dockerfile.windowsservercore-10.0.17666" -t "$repo/windows-ad:impersonate-backend-windowsservercore-insider-10.0.17666.1000" .
+docker build -f "windows-auth-impersonate-explicit-frontend/Dockerfile.windowsservercore-10.0.17666" -t "$repo/windows-ad:impersonate-explicit-frontend-windowsservercore-insider-10.0.17666.1000" .
+docker build -f "windows-auth-impersonate-groupupn-backend/Dockerfile.windowsservercore-10.0.17666" -t "$repo/windows-ad:impersonate-groupupn-backend-windowsservercore-insider-10.0.17666.1000" .
+docker build -f "windows-auth-impersonate-globalasax-backend/Dockerfile.windowsservercore-10.0.17666" -t "$repo/windows-ad:impersonate-globalasax-backend-windowsservercore-insider-10.0.17666.1000" .
